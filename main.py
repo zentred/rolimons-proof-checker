74e2e7d8b3060bc2551e36d86b04897e07788593
zentred <87871486+zentred@users.noreply.github.com>
2023-02-14 18:05:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 @commands.cooldown(1, 5, commands.BucketType.user)
 async def proof(ctx, *, args):
+    if any(ctx.channel.id == int(i) for i in config['ignoreChannels']): return None
     itemName, itemAcronym, amount = Tools.findItem(args, acroToName, nameToAcro, noAcronym)
     offset = 0
 
@@ -58,6 +59,12 @@ async def proof(ctx, *, args):
         content = Tools.toUse(itemAcronym, itemName)
         r = session.get(f'https://discord.com/api/v9/guilds/415246288779608064/messages/search?channel_id=535250426061258753&content={content}&offset={offset}').json()
 
+        if 'You are being rate limited.' in str(r):
+            embed = discord.Embed(title = 'Ratelimited', description = f'Retrying in 15 seconds', color = 0xf08a00)
+            await ctx.channel.send(embed=embed)
+            time.sleep(15)
+            continue
+
         if r['total_results'] == 0 or r['messages'] == []:
             embed = discord.Embed(title = 'No proofs', description = f'Unable to find *more* proofs for `{args}`', color = 0xFF0000)
             await ctx.channel.send(embed=embed)
@@ -65,31 +72,61 @@ async def proof(ctx, *, args):
 
         for proof in r['messages']:
             try:
-                imageUrl = proof[0]['attachments'][0]['url']
-                Message = proof[0]['content']
-                splitMessage = Message.lower().splitlines()
-                firstLine = firstLineGrab(splitMessage)
+                if proof[0]['author']['id'] != '1034147338689724508':
+                    imageUrl = proof[0]['attachments'][0]['url']
+                    Message = proof[0]['content']
+                    splitMessage = Message.lower().splitlines()
+                    firstLine = firstLineGrab(splitMessage)
 
-                if itemAcronym != None:
-                    if len(itemAcronym.split()) > 1:
-                        if len(set(itemAcronym.split())&set(firstLine.split())) == len(itemAcronym.split()): pass
-                        elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
+                    if itemAcronym != None:
+                        if len(itemAcronym.split()) > 1:
+                            if len(set(itemAcronym.split())&set(firstLine.split())) == len(itemAcronym.split()): pass
+                            elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
+                            else: continue
+                        else:
+                            if len(set(itemAcronym.split())&set(firstLine.split())) == 1: pass
+                            elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
+                            else: continue
+
+
+                    elif itemAcronym == None:
+                        if len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
                         else: continue
-                    else:
-                        if len(set(itemAcronym.split())&set(firstLine.split())) == 1: pass
-                        elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
+
+                    embed = discord.Embed(color = 0x4042CE)
+                    datePhrase, opAmount = Tools.filter(proof[0]['timestamp'], splitMessage)
+                    embed.add_field(name = datePhrase, value = Message, inline = True)
+                    embed.add_field(name = 'Image URL', value = f'[Click Here]({imageUrl}){opAmount}')
+                    embed.set_thumbnail(url = imageUrl)
+                else:
+                    firstLine = firstLineGrab([proof[0]['embeds'][0]['description'].lower().replace('**', '').split('item: ')[1].split('\n')[0]])
+
+                    if itemAcronym != None:
+                        if len(itemAcronym.split()) > 1:
+                            if len(set(itemAcronym.split())&set(firstLine.split())) == len(itemAcronym.split()): pass
+                            elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
+                            else: continue
+                        else:
+                            if len(set(itemAcronym.split())&set(firstLine.split())) == 1: pass
+                            elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
+                            else: continue
+
+
+                    elif itemAcronym == None:
+                        if len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
                         else: continue
+                    
 
-
-                elif itemAcronym == None:
-                    if len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
-                    else: continue
-
-                embed = discord.Embed(color = 0x4042CE)
-                datePhrase, opAmount = Tools.filter(proof[0]['timestamp'], splitMessage)
-                embed.add_field(name = datePhrase, value = Message, inline = True)
-                embed.add_field(name = 'Image URL', value = f'[Click Here]({imageUrl}){opAmount}')
-                embed.set_thumbnail(url = imageUrl)
+                    embed = discord.Embed(color = 0x000000)
+                    imageUrl = proof[0]['embeds'][0]['image']['proxy_url']
+                    datePhrase, opAmount = Tools.filter(proof[0]['timestamp'], None)
+                    opAmount = proof[0]['embeds'][0]['description'].lower().split('**value**: ')[1].split('\n')[0]
+                    opAmount = f'\n\n**Proof Amount**\n{opAmount}'
+                    embed.add_field(name = datePhrase, value = proof[0]['embeds'][0]['description'], inline = True)
+                    embed.add_field(name = 'Image URL', value = f'[Click Here]({imageUrl}){opAmount}')
+                    footer = proof[0]['embeds'][0]['description'].lower().split('\n')[0].replace('<', '').replace('>', '').replace('@!', '')
+                    embed.set_footer(text=f"Blacklisted {footer}")
+                    embed.set_thumbnail(url = imageUrl)
                 await ctx.channel.send(embed=embed)
                 [(foundProofs := foundProofs + 1, foundProofsTotal := foundProofsTotal + 1)]
 
@@ -106,78 +143,12 @@ async def proof(ctx, *, args):
                     if msg.content.lower() == 'more': foundProofs -= 5; proofsToFind += 5; continue
                     else: return None
 
-            except Exception as err:
-                print(err)
+            except:
                 pass
 
         if foundProofs & 5 != 0 or foundProofs == 0 or foundProofsTotal != proofsToFind:
             offset += 25
             continue
         break
-
-@bot.command()
-@commands.cooldown(1, 5, commands.BucketType.user)
-async def often(ctx, *, args):
-    itemName, itemAcronym, amount = Tools.findItem(args, acroToName, nameToAcro, noAcronym)
-
-    if amount != 1:
-        if amount > 1: embed = discord.Embed(title = 'Not specific enough', description = f'`{args}` matched with multiple other limiteds, retry using the limited acronym or **full** name', color = 0xff0000)
-        elif amount == 0: embed = discord.Embed(title = "No found limited", description = f'Unable to find limited to match with `{args}`', color = 0xff0000)
-        await ctx.channel.send(embed=embed)
-        return None
-
-    offset = 0
-    timeStamps = []
-    imageUrl = requests.get(f'https://thumbnails.roblox.com/v1/assets?assetIds={assetIds[str(itemName)]}&returnPolicy=0&size=250x250&format=Png&isCircular=false').json()['data'][0]['imageUrl']
-
-    while len(timeStamps) != 10:
-        content = Tools.toUse(itemAcronym, itemName)
-        r = session.get(f'https://discord.com/api/v9/guilds/415246288779608064/messages/search?channel_id=535250426061258753&content={content}&offset={offset}').json()
-
-        if r['total_results'] == 0 or r['messages'] == []:
-            break
-
-        else:
-            for proof in r['messages']:
-                if len(timeStamps) != 10:
-                    try:
-                        Message = proof[0]['content']
-                        splitMessage = Message.lower().splitlines()
-                        firstLine = firstLineGrab(splitMessage)
-
-                        if itemAcronym != None:
-                            if len(itemAcronym.split()) > 1:
-                                if len(set(itemAcronym.split())&set(firstLine.split())) == len(itemAcronym.split()): pass
-                                elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
-                                else: continue
-                            else:
-                                if len(set(itemAcronym.split())&set(firstLine.split())) == 1: pass
-                                elif len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
-                                else: continue
-
-
-                        elif itemAcronym == None:
-                            if len(set(itemName.split())&set(firstLine.split())) == len(itemName.split()): pass
-                            else: continue
-
-                        timeStamps.append(proof[0]['timestamp'])
-                    except:
-                        pass
-            offset += 25
-
-    if len(timeStamps) >= 2:
-        filteredDates, sinceRecent, betweenDates = Tools.checkDates(timeStamps)
-        embed = discord.Embed(title = args, description = f'days between the {len(filteredDates)} recent proofs: **{betweenDates}**\ndays since the most recent proof: **{sinceRecent}**', color = 0x4042CE)
-        embed.set_thumbnail(url = imageUrl)
-        await ctx.channel.send(embed=embed)
-    elif len(timeStamps) == 1:
-        filteredDates, sinceRecent, betweenDates = Tools.checkDates(timeStamps)
-        embed = discord.Embed(title = args, description = f'managed to only find **1** proof. unable to continue', color = 0x4042CE)
-        embed.set_thumbnail(url = imageUrl)
-        await ctx.channel.send(embed=embed)
-    else:
-        embed = discord.Embed(title = 'No proofs', description = f'Unable to find *more* proofs for `{args}`', color = 0xFF0000)
-        embed.set_thumbnail(url = imageUrl)
-        await ctx.channel.send(embed=embed)
 
 bot.run(config['discordBotToken'])
